cf8a74a10bc4cd6184f21ade9161197bef59f73d
harrison.kim <harrison.kim@kakaocorp.com>
2019-12-18 14:53:14 +0900

    new

---
+++ src/test/java/com/project/elevator/ElevatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.elevator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
